4612a153b595966dab8a5b1f5d8545b03205ba6d
Sumaita Sabaha <sumaitasabaha9@gmail.com>
2021-08-24 23:17:46 -0500

    Imported images, made home page, added initial styles.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/sumi.css';
+import './styles/main_styles.css';
+import MyButton from './components/MyButton.js';
+import { FiMail, FiInstagram, FiFacebook, FiGithub} from 'react-icons/fi';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "main_container">
+        <div className= "section1">
+            <img src= "./images/sky.png" alt="" className= "sky"></img>
+            <img src= "./images/clouds.png" alt="" className= "clouds"></img>
+            <img src= "./images/front.png" alt="" className= "front"></img>
+            <img src= "./images/buildings.png" alt="" className= "buildings"></img>
+            <img src= "./images/building.png" alt="" className= "building"></img>
+            
+        </div>
+
+        <div className= "section2">
+          <svg className="svg_curve_top" xmlns="http://www.w3.org/2000/svg" fill="#051d21" viewBox="0 0 100 100" preserveAspectRatio="none" >
+            <path d="M0,100 C65,93 76,10 100,100" />
+          </svg>
+          <div className= "section2_container">
+              <div className= "left_side">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum, nostrum veniam soluta optio a reiciendis labore consequatur nesciunt ipsam </p>
+              </div>
+
+              <div className= "right_side">
+                  <img src= "https://picsum.photos/1200/1200" alt="" className="image"/>
+              </div>
+          </div>
+
+          <svg className="svg_curve_bottom" xmlns="http://www.w3.org/2000/svg" fill="#051d21" viewBox="0 0 100 100" preserveAspectRatio="none" >
+            <path d="M0,0 C65,20 90,5 100,0" />
+          </svg>
+
+        </div>
+
+        <div className= "section3">
+            contact me!
+            <input type = "text" name= "email" id = "email" className= "input_email"/>
+            <MyButton />
+
+            <div className= "contacts">
+              <div className= "contact_section">
+                <ul>
+                    <li><FiMail/> Email</li>
+                </ul>
+
+              </div>
+
+              <div className= "contact_section">
+              <ul>
+                  <li><FiInstagram style={{color:'white'}}/> Instagram</li>
+                  <li><FiFacebook/> Facebook</li>
+                  <li><FiGithub/> Github</li>
+              </ul>
+              </div>
+
+            </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/MyButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyButton() {
+    return (
+        <div>
+            MyButton
+        </div>
+    )
+}
+
+export default MyButton;
